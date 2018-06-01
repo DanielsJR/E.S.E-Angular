ce4388b65288ea9ac0328deca5001c0dc1c28381
Daniels <romulachop@yahoo.cl>
2018-06-01 17:31:42 -0400

    set roles reactive forms

--- src/app/home/get-users/set-roles-dialog/set-roles-dialog-ref/set-roles-dialog-ref.component.ts
+++ src/app/home/get-users/set-roles-dialog/set-roles-dialog-ref/set-roles-dialog-ref.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Inject, ViewChild, AfterContentInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { MatDialogRef, MAT_DIALOG_DATA, MatSlideToggle, MatChip } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { URI_MANAGERS, URI_TEACHERS, ROLE_MANAGER, ROLE_TEACHER, ROLE_ADMIN, ROLE_ADMIN_SPANISH, ROLE_MANAGER_SPANISH, ROLE_TEACHER_SPANISH, ROLE_STUDENT, ROLE_STUDENT_SPANISH } from '../../../../app.config';
 import { User } from '../../../../models/user';
 import { ManagerStoreService } from '../../../../services/manger-store.service';
@@ -8,6 +8,7 @@ import { TeacherStoreService } from '../../../../services/teacher-store.service'
 import { StudentStoreService } from '../../../../services/student-store.service';
 import { PRIVILEGES } from '../../../../models/privileges';
 import { Subscription } from 'rxjs';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -15,50 +16,40 @@ import { Subscription } from 'rxjs';
   templateUrl: './set-roles-dialog-ref.component.html',
   styleUrls: ['./set-roles-dialog-ref.component.css']
 })
-export class SetRolesDialogRefComponent implements OnInit, AfterContentInit, OnDestroy {
+export class SetRolesDialogRefComponent implements OnInit, OnDestroy {
 
   uriRole: any;
   user: User;
   rolesList = PRIVILEGES;
-
-  
+  setRolesForm: FormGroup;
   subscriptionsetRoles: Subscription;
 
-  @ViewChild('slideToggleManager') slideToggleManager: MatSlideToggle;
-  @ViewChild('slideToggleTeacher') slideToggleTeacher: MatSlideToggle;
 
   constructor(public dialogRef: MatDialogRef<SetRolesDialogRefComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private managerStoreService: ManagerStoreService,
-    private teacherStoreService: TeacherStoreService,
-    private studentStoreService: StudentStoreService,
+    private managerStoreService: ManagerStoreService, private teacherStoreService: TeacherStoreService,
+    private studentStoreService: StudentStoreService, private formBuilder: FormBuilder,
     public sanitizer: DomSanitizer) {
 
     this.user = data.user;
     this.uriRole = this.data.uriRole;
 
-  
 
     console.log('Dialog*** UserName: ' + data.user.firstName + ' uriRol: ' + data.uriRole + ' type: ' + data.type);
   }
 
-
-  ngAfterContentInit(): void {
-    (this.user.roles.includes(ROLE_MANAGER)) ? this.slideToggleManager.checked = true : false;
-    (this.user.roles.includes(ROLE_TEACHER)) ? this.slideToggleTeacher.checked = true : false;
-  }
-
   ngOnInit(): void {
+    this.buildForm();
 
     if (this.uriRole === URI_MANAGERS) {
-    this.subscriptionsetRoles =  this.managerStoreService.setRoles$.subscribe(user => {
+      this.subscriptionsetRoles = this.managerStoreService.setRoles$.subscribe(user => {
         this.teacherStoreService.updateUserFromStore(user);
         this.dialogRef.close('set');
       });
 
 
     } else if (this.uriRole === URI_TEACHERS) {
-      this.subscriptionsetRoles =  this.teacherStoreService.setRoles$.subscribe(user => {
+      this.subscriptionsetRoles = this.teacherStoreService.setRoles$.subscribe(user => {
         this.managerStoreService.updateUserFromStore(user);
         this.dialogRef.close('set');
 
@@ -74,10 +65,17 @@ export class SetRolesDialogRefComponent implements OnInit, AfterContentInit, OnD
     this.subscriptionsetRoles.unsubscribe();
   }
 
+  buildForm() {
+    this.setRolesForm = this.formBuilder.group({
+      toggleManager: [(this.user.roles.includes(ROLE_MANAGER)) ? true : false],
+      toggleTeacher: [(this.user.roles.includes(ROLE_TEACHER)) ? true : false],
+    });
+  }
+
   setRoles(): void {
     let roles: string[] = [];
-    if (this.slideToggleManager.checked) roles.push(ROLE_MANAGER);
-    if (this.slideToggleTeacher.checked) roles.push(ROLE_TEACHER);
+    if (this.setRolesForm.get('toggleManager').value === true) roles.push(ROLE_MANAGER);
+    if (this.setRolesForm.get('toggleTeacher').value === true) roles.push(ROLE_TEACHER);
     if (roles.length > 0) this.user.roles = roles;
 
     if (this.uriRole === URI_MANAGERS) {
