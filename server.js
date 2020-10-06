fbd5988c621cc8a12a7d38cbbc2450532589bb06
Daniels <d.rubio@outlook.com>
2020-10-06 12:18:28 -0300

    heroku deploy

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist'));
+app.use(express.static('./dist/ese'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist' }
+  res.sendFile('index.html', { root: 'dist/ese' }
   );
 });
 
